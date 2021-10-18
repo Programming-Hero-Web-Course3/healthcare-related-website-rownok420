327bd9831616179a8bcef714cc4817ac99695cb9
rownok420 <mrbluebird420@gmail.com>
2021-10-18 16:22:35 +0600

    firebase init

--- src/Pages/Login/Firebase/Firebase.config.js
+++ src/Pages/Login/Firebase/Firebase.config.js
@@ -1,4 +1,4 @@
-console.log(process.env.REACT_APP_FIREBASE_API_KEY)
+console.log(process.env.REACT_APP_FIREBASE_API_KEY);
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -7,30 +7,22 @@ const firebaseConfig = {
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
+
 };
 
 export default firebaseConfig;
 
-// apiKey: "REACT_APP_FIREBASE_API_KEY",
-//     authDomain: "REACT_APP_FIREBASE_AUTH_DOMAIN",
-//     projectId: "REACT_APP_FIREBASE_PROJECT_ID",
-//     storageBucket: "REACT_APP_FIREBASE_STORAGE_BUCKET",
-//     messagingSenderId: "REACT_APP_FIREBASE_MESSAGING_SENDER_ID",
-//     appId: "REACT_APP_FIREBASE_APP_ID"
-
-// apiKey: "AIzaSyAPpJUSlmVQwxad6etJSuAoemnalyQ8cuo",
-//   authDomain: "simple-firebase-authenti-f7f79.firebaseapp.com",
-//   projectId: "simple-firebase-authenti-f7f79",
-//   storageBucket: "simple-firebase-authenti-f7f79.appspot.com",
-//   messagingSenderId: "572836130364",
-//   appId: "1:572836130364:web:0584f6ae08fa4c1919dd39"
+// apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_FIREBASE_APP_ID,
 
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAPpJUSlmVQwxad6etJSuAoemnalyQ8cuo",
-//   authDomain: "simple-firebase-authenti-f7f79.firebaseapp.com",
-//   projectId: "simple-firebase-authenti-f7f79",
-//   storageBucket: "simple-firebase-authenti-f7f79.appspot.com",
-//   messagingSenderId: "572836130364",
-//   appId: "1:572836130364:web:0584f6ae08fa4c1919dd39"
-// };
+// apiKey: "AIzaSyCGJDqDp0Bq6jNy-PBeT1dfEhjJiefzzvg",
+//     authDomain: "health-care-ec6f7.firebaseapp.com",
+//     projectId: "health-care-ec6f7",
+//     storageBucket: "health-care-ec6f7.appspot.com",
+//     messagingSenderId: "906565328365",
+//     appId: "1:906565328365:web:c644571eb53a60f2731be4",
